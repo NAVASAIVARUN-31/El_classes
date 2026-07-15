5a35d3ce45748adc73af896123417077b69c49af
NAVASAIVARUN-31 <varunnnn31@gmail.com>
2026-07-15 11:16:26 +0530

    initial commit

---
+++ function.js
@@ -0,0 +1,13 @@
+(function(){
+    let exam
+Duration = 620;
+    let totalQuestions = 50;
+
+    console.log("Exam settings loaded");
+    console.log("Exam Duration: " , examDuration , " minutes");
+    console.log("Total Questions: " , totalQuestions);
+})();
+
+(function () {
+    console.log("Runs immediately");
+})();
